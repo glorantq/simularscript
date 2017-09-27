48b6298002a09874229bad2823d1d719b6e108c3
Gerber Lóránt Viktor <glorantq@gmail.com>
2017-09-27 18:37:13 +0200

    LibGDX base

---
+++ android/src/io/glorantq/simularscript/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.glorantq.simularscript;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.glorantq.simularscript.SSEngine;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SSEngine(), config);
+	}
+}
